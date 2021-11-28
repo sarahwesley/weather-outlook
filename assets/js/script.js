07417a9ed499e647499aef47fca4ad61b4401f74
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-28 13:37:27 -0500

    api key fixed, past searches show in column, wind speed added

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,6 @@ var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
-// save search to local storage
-var saveSearch = function() {
-    localStorage.setItem("cities", JSON.stringify(cities));
-}
-
 // form to enter a city
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -31,13 +26,17 @@ var formSubmitHandler = function(event) {
     pastSearch(city);
 }
 
+// save search to local storage
+var saveSearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
 // fetch weather data from api and create api key variable
 var getCityWeather = function(city) {
-    var apiKey = "524901&appid=51e858bcb3c9554aaabd869dfdb30a58";
+    var apiKey = "51e858bcb3c9554aaabd869dfdb30a58";
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-    fetch(apiUrl) 
-    .then(function(response) {
+    fetch(apiURL).then(function(response) {
         response.json().then(function(data){
             displayWeather(data, city);
         });
@@ -47,6 +46,7 @@ var getCityWeather = function(city) {
 
 // fucntion to dispay pulled data
 var displayWeather = function(weather, searchCity) {
+    console.log(weather);
 
     // show current date
     var currentDate = document.createElement("span")
@@ -55,12 +55,12 @@ var displayWeather = function(weather, searchCity) {
 
     // show temputure data
     var temperatureEl = document.createElement("span");
-    temperatureEl.textContent = "temperature: " + weather.main.temp + "Farenhait";
+    temperatureEl.textContent = "temperature: " + weather.main.temp + " °F";
     temperatureEl.classList = "list-group-item"
 
     // show humidity data
     var humidityEl = document.createElement("span");
-    humidityEl.textContent = "humidity: " + weather.main.humidity + " %";
+    humidityEl.textContent = "humidity: " + weather.main.humidity + "%";
     humidityEl.classList = "list-group-item"
 
     // show wind data
@@ -79,6 +79,19 @@ var displayWeather = function(weather, searchCity) {
     getUvIndex(lat,lon)
 }
 
+
+// get uv index function
+
+var getUvIndex = function(lat,lon) {
+    var apiKey = "51e858bcb3c9554aaabd869dfdb30a58";
+    var apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
+    fetch(apiURL)
+    .then(function(response) {
+        response.json().then(function(data){
+            displayUvIndex(data)
+        });
+    });
+}
 // uv index function
 var displayUvIndex = function(index){
     var uvIndexEl = document.createElement("div");
@@ -91,7 +104,7 @@ var displayUvIndex = function(index){
     if(index.value <= 2) {
         uvIndexValue.classList = "favorable"
     } else if(index.value > 2 && index.value <= 8){
-        uvIndexValue.classList = "moderate "
+        uvIndexValue.classList = "moderate"
     } else if(index.value > 8){
         uvIndexValue.classList = "severe"
     };
@@ -102,21 +115,90 @@ var displayUvIndex = function(index){
 }
 
 // fetch data for 5 day forecast
-var get5Day = function (city) {
-    var apiKey = "524901&appid=51e858bcb3c9554aaabd869dfdb30a58";
+var get5Day = function(city) {
+    var apiKey = "51e858bcb3c9554aaabd869dfdb30a58";
     var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
     fetch(apiURL)
     .then(function(response) {
-        response.json().then(fucntion(data) {
+        response.json().then(function(data) {
             display5Day(data);
         });
     });
 };
 
-// TODO display data fro 5 day forecast
+// display results for 5 day forecast
+var display5Day = function(weather) {
+    forecastContainerEl.textContent = ""
+    forecastTitle.textContent = "Your 5 Day Forecast";
 
-// TODO create function to show past searches. show in past search bar once submitting "city" in search bar
+    var forecast = weather.list;
+    for (var i=5; i < forecast.length; i=i+8){
+    var dailyForecast = forecast[i];
+        
+    var forecastEl=document.createElement("div");
+    forecastEl.classList = "card bg-secondary text-light m-2";
 
-// create click and submit event handlers
+       
 
+    // date element
+    var forecastDate = document.createElement("h5")
+    forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");       
+    forecastDate.classList = "card-header text-center bg-primary"
+    forecastEl.appendChild(forecastDate);
+ 
+       
+    // temperture element
+    var forecastTempEl=document.createElement("span");
+    forecastTempEl.classList = "card-body text-center";
+    forecastTempEl.textContent = "temp: " + dailyForecast.main.temp + " °F";
+        
+    forecastEl.appendChild(forecastTempEl);
+    
+    // humidity element
+    var forecastHumEl=document.createElement("span");
+    forecastHumEl.classList = "card-body text-center";
+    forecastHumEl.textContent = "humidity: " + dailyForecast.main.humidity + "%";
+
+    forecastEl.appendChild(forecastHumEl);
+
+    // wind speed
+    var forecastWindEl = document.createElement("span");
+    forecastWindEl.classList = "card-body text-center";
+    forecastWindEl.textContent = "wind speed: " + dailyForecast.wind.speed + " MPH";
+
+    forecastEl.appendChild(forecastWindEl);
+
+
+
+    forecastContainerEl.appendChild(forecastEl);
+    }
+
+}
+
+
+// save searches in past search bar
+var pastSearch = function(pastSearch){
+
+    pastSearchEl = document.createElement("button");
+    pastSearchEl.textContent = pastSearch;
+    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    pastSearchEl.setAttribute("data-city",pastSearch)
+    pastSearchEl.setAttribute("type", "submit");
+
+    pastSearchButtonEl.prepend(pastSearchEl);
+}
+
+// when "past searches" are clicked on, show data for that city
+
+var pastSearchHandler = function(event){
+    var city = event.target.getAttribute("data-city")
+    if(city) {
+        getCityWeather(city);
+        get5Day(city);
+    }
+}
+
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
+pastSearchButtonEl.addEventListener("click", pastSearchHandler);
